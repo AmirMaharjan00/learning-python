a20e74d890ed1462518f6f2d85ad9719925b6f18
Amir Maharjan <mhrznamir.am@gmail.com>
2024-12-10 20:51:44 +0545

    dashboard update

---
+++ college_management/members/templates/dashboard/dashboard.py
@@ -0,0 +1,13 @@
+"""
+Dashboard Head
+
+@since 1.0.0
+"""
+def dashboard_head():
+    html = f"""
+        <div class="dashboard-head">
+            <div class="dashboard-intro"></div>
+            <div class="dashboard-actions"></div>
+        </div>
+    """
+    return ''
